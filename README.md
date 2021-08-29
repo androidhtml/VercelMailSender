2ebbb3e5b6f946e23055a6f045213c86ba8aa209
lbr77 <2298454647@qq.com>
2021-08-29 20:10:06 +0800

    Inital Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+VERCEL邮件通知（
+
+配置环境变量
+
+```python
+"passwd": "",
+"MASTER_NAME": "",
+"SMTP_SERVER": "",
+"SMTP_USER": "",
+"SMTP_PASS": "",
+"RECIEVER": ""
+```
+
---
+++ api/index.py
@@ -0,0 +1,63 @@
+'''
+Date: 2021-08-28 19:32:06
+LastEditors: Steve Li
+LastEditTime: 2021-08-29 20:06:51
+FilePath: \pythonBlogd:\Programming\python\mail\api\index.py
+'''
+import smtplib as ml
+from http.server import BaseHTTPRequestHandler
+# from os import environ
+from json import dumps,loads
+from re import compile
+from email.mime.text import MIMEText
+from email.header import Header
+from urllib.parse import unquote
+from email.utils import formataddr
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin',"*")
+            self.send_header('Content-type','application/json')
+            self.end_headers();
+            passwd = compile(r'passwd=([^?&=]*)').findall(self.path);
+            if len(passwd)==0:
+                self.wfile.write(dumps({
+                        "code": 0,
+                        "msg": "请输入密码"
+                }).encode());
+                return;
+            elif passwd[0]=='' or passwd[0] != environ['passwd']:
+                self.wfile.write(dumps({
+                        "code": 0,
+                        "msg": "密码错误"
+                    }).encode(encoding='utf-8'));
+                return;
+            msg = compile(r'msg=([^?&=]*)').findall(self.path);
+            if len(msg)==0:
+                    self.wfile.write(dumps({
+                        "code": 0,
+                        "msg": "未指定消息"
+                    }).encode(encoding='utf-8'));
+                    return;
+            content =  MIMEText(unquote(msg[0]),'plain','utf-8')
+            content['From']=formataddr(["Vercel通知小助手",environ["SMTP_USER"]])
+            content['To']=formataddr([environ["MASTER_NAME"],environ["RECIEVER"]]);
+            content['Subject']=Header("有新通知辣!");
+            smtpobj = ml.SMTP_SSL(environ["SMTP_SERVER"],465)
+            smtpobj.login(environ["SMTP_USER"],environ["SMTP_PASS"]);
+            mail = content
+            smtpobj.send_message(from_addr=environ["SMTP_USER"], to_addrs=environ["RECIEVER"],msg=mail)
+            self.wfile.write(dumps({
+                        "code": 200,
+                        "msg": "成功"
+                    }).encode(encoding='utf-8'));
+            return;
+        except Exception as e:
+            print(e);
+            self.wfile.write(dumps({
+                "code": 500,
+                "msg": "配置错误，请在服务端查看错误日志"
+            }).encode(encoding='utf-8'))
+        finally:
+            return;
